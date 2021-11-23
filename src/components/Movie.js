1ea136c38bf2aa5fa7b6a3ebea19e6714200f066
neautish <75166589+neautish@users.noreply.github.com>
2021-11-23 14:58:05 +0330

    some ui changes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -20,7 +20,7 @@ const Movie = (props) => {
             <img className={Classes.img} src={props.poster} alt={props.title} />
             <div className={Classes.movie_details}>
                 <h2 className={Classes.movie_details__title}>{props.title}</h2>
-                <span style={{ color: Rate(props.score) }} className={Classes.movie_details__score}>{props.score}</span>
+                <span style={{ backgroundColor: Rate(props.score) }} className={Classes.movie_details__score}>{props.score}</span>
                 {console.log(`Classes.movie_details__score ${Rate(props.score)}`)}
             </div>
             <div className={Classes.movie_overview}>
