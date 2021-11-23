f56d7fae86e7cb4ee03b79b3a7c30e305f36b8fb
neautish <75166589+neautish@users.noreply.github.com>
2021-11-23 12:47:39 +0330

    add color to different scores

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -3,6 +3,16 @@ import React from "react";
 import Classes from './Movie.module.css';
 
 const Movie = (props) => {
+    const Rate = (score) => {
+        if (score > 7) {
+            return "green";
+        } else if (score >= 5) {
+            return "orange";
+        } else {
+            return "red";
+        }
+    };
+
     return (
         <div
             className={Classes.movie}
@@ -10,7 +20,8 @@ const Movie = (props) => {
             <img className={Classes.img} src={props.poster} alt={props.title} />
             <div className={Classes.movie_details}>
                 <h2 className={Classes.movie_details__title}>{props.title}</h2>
-                <span className={Classes.movie_details__score}>{props.score}</span>
+                <span style={{ color: Rate(props.score) }} className={Classes.movie_details__score}>{props.score}</span>
+                {console.log(`Classes.movie_details__score ${Rate(props.score)}`)}
             </div>
             <div className={Classes.movie_overview}>
                 <h3 className={Classes.movie_overview__title}>Overview</h3>
